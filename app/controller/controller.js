60770039f3f062eaf18e77b3b7712ca28e140047
Scott Gillatt <scottgillatt@gmail.com>
2016-02-24 21:07:22 -0700

    initiate

---
+++ app/controller/controller.js
@@ -0,0 +1,41 @@
+/* global angular */
+app.controller('RosterController', function ($scope, DataService, $filter) {
+    $scope.players = [];
+    $scope.roster = [];
+
+    $scope.loadPlayers = function () {
+        DataService.loadPlayers();
+    },
+
+    $scope.setPlayers = function () {
+        $scope.players = DataService.getAllPlayers();
+    }
+
+    $scope.addPlayers = function () {
+ 
+        $scope.roster.push(
+            {
+                fullname: $scope.newplayer.fullname,
+                jersey: $scope.newplayer.jersey,
+                position: $scope.newplayer.position,
+                pro_team: $scope.newplayer.pro_team
+            })
+        $scope.resetPlayer();
+    }
+    
+    $scope.addToRoster = function (player) {
+        $scope.roster.push(player)
+        $scope.players.splice($scope.players.indexOf(player), 1);
+    }
+
+    $scope.resetPlayer = function () {
+        $scope.newplayer = "";
+    }
+
+    $scope.removeFromRoster = function (player) {
+        $scope.players.push(player)
+        $scope.roster.splice($scope.roster.indexOf(player), 1);
+    }
+
+
+});
